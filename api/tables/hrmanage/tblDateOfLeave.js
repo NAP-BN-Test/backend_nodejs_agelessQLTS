6d15fe4c017cc31256596061aa7eb706ad75e54d
Tung Nguyen <NAP-BN-Test>
2021-05-17 10:27:44 +0700

    update

---
+++ api/tables/hrmanage/tblDateOfLeave.js
@@ -0,0 +1,17 @@
+const Sequelize = require('sequelize');
+
+module.exports = function(db) {
+    var table = db.define('tblDateOfLeave', {
+        ID: {
+            type: Sequelize.BIGINT,
+            primaryKey: true,
+            autoIncrement: true
+        },
+        LeaveID: Sequelize.BIGINT,
+        DateEnd: Sequelize.NOW,
+        DateStart: Sequelize.NOW,
+
+    });
+
+    return table;
+}
