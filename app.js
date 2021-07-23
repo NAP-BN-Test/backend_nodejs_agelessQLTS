5bb7bf9ffa8b9171a4662b515b125f3547e0d6ef
NAP-BN-Test <caonv174@gmail.com>
2021-07-23 13:37:06 +0700

    đỏi port

--- app.js
+++ app.js
@@ -59,7 +59,7 @@ let storage = multer.diskStorage({
 let upload = multer({ storage: storage });
 const DIR = 'C:/images_services/ageless_sendmail';
 
-app.post('/qlnb/upload', getDateInt, upload.array('photo', 12), async function(req, res) {
+app.post('/qlnb/upload', getDateInt, upload.array('photo', 12), async function (req, res) {
     if (!req.files) {
         console.log("No file received");
         return res.send({
@@ -79,7 +79,7 @@ app.post('/qlnb/upload', getDateInt, upload.array('photo', 12), async function(r
 
                 var apiInstance = new CloudmersiveConvertApiClient.ConvertDocumentApi();
                 var inputFile = Buffer.from(fs.readFileSync(pathFirst).buffer); // File | Input file to perform the operation on.
-                var callback = function(error, data, response) {
+                var callback = function (error, data, response) {
                     if (error) {
                         console.error(error);
                     } else {
@@ -145,30 +145,30 @@ async function handleRequestShopping(db, idycms) {
             ['ID', 'DESC']
         ],
         include: [{
-                model: mtblDMBoPhan(db),
-                required: false,
-                as: 'phongban'
-            },
-            {
-                model: mtblDMNhanvien(db),
-                required: false,
-                as: 'NhanVien'
-            },
-            {
-                model: mtblDMNhanvien(db),
-                required: false,
-                as: 'PheDuyet1',
-            },
-            {
-                model: mtblDMNhanvien(db),
-                required: false,
-                as: 'PheDuyet2',
-            },
-            {
-                model: tblYeuCauMuaSamDetail,
-                required: false,
-                as: 'line'
-            },
+            model: mtblDMBoPhan(db),
+            required: false,
+            as: 'phongban'
+        },
+        {
+            model: mtblDMNhanvien(db),
+            required: false,
+            as: 'NhanVien'
+        },
+        {
+            model: mtblDMNhanvien(db),
+            required: false,
+            as: 'PheDuyet1',
+        },
+        {
+            model: mtblDMNhanvien(db),
+            required: false,
+            as: 'PheDuyet2',
+        },
+        {
+            model: tblYeuCauMuaSamDetail,
+            required: false,
+            as: 'line'
+        },
         ],
         where: { ID: idycms }
     }).then(async data => {
@@ -192,7 +192,7 @@ async function handleRequestShopping(db, idycms) {
                         model: mtblDMLoaiTaiSan(db),
                         required: false,
                         as: 'loaiTaiSan'
-                    }, ],
+                    },],
                 }).then(data => {
                     if (data) {
                         name += ',' + data ? data.Name : ''
@@ -245,30 +245,30 @@ async function handlePaymentOrder(db, iddntt) {
             ['ID', 'DESC']
         ],
         include: [{
-                model: tblDMNhanvien,
+            model: tblDMNhanvien,
+            required: false,
+            as: 'NhanVien',
+            include: [{
+                model: tblDMBoPhan,
                 required: false,
-                as: 'NhanVien',
+                as: 'bophan',
                 include: [{
-                    model: tblDMBoPhan,
+                    model: mtblDMChiNhanh(db),
                     required: false,
-                    as: 'bophan',
-                    include: [{
-                        model: mtblDMChiNhanh(db),
-                        required: false,
-                        as: 'chinhanh'
-                    }, ],
-                }, ],
-            },
-            {
-                model: mtblDMNhanvien(db),
-                required: false,
-                as: 'KTPD'
-            },
-            {
-                model: mtblDMNhanvien(db),
-                required: false,
-                as: 'LDPD'
-            },
+                    as: 'chinhanh'
+                },],
+            },],
+        },
+        {
+            model: mtblDMNhanvien(db),
+            required: false,
+            as: 'KTPD'
+        },
+        {
+            model: mtblDMNhanvien(db),
+            required: false,
+            as: 'LDPD'
+        },
         ],
     }).then(async data => {
         objKey = {
@@ -294,7 +294,7 @@ async function getPathFromtblTmplate(db, code, idycms) {
             model: mtblFileAttach(db),
             required: false,
             as: 'tem'
-        }, ],
+        },],
     }).then(data => {
         //  data.tem[0].Link.slice(44, 100)
         pathFirst = data.tem[0].Link.slice(47, 100);
@@ -305,55 +305,55 @@ async function getPathFromtblTmplate(db, code, idycms) {
     return pathFirst
 }
 // -----------------------------------------------------------------------------------------------------------------------------------------------------------------------------
-app.post('/qlnb/render_automatic_work', async function(req, res) {
-        let body = req.body;
-        var pathFirst = '';
-        var objKey = {};
-        await database.connectDatabase().then(async db => {
-            pathFirst = await getPathFromtblTmplate(db, body.code, body.id)
-            if (body.code == 'ycms') {
-                objKey = await handleRequestShopping(db, body.id)
-            } else if (body.code == 'dntt') {
-                objKey = await handlePaymentOrder(db, body.id)
-            } else {
-                objKey = await handleRequestShopping(db, body.id)
-            }
-        })
-        var pathTo = 'C:/images_services/ageless_sendmail/'
-        console.log(pathTo + pathFirst);
-        fs.readFile(pathTo + pathFirst, 'binary', function(err, data) {
-            try {
-                if (err) {
-                    console.log(err);
-                    var result = {
-                        status: Constant.STATUS.FAIL,
-                        message: 'File không tồn tại. Vui lòng cầu hình lại!',
-                    }
-                    res.json(result);
-                } else {
-                    var zip = new JSZip(data);
-                    var doc = new Docxtemplater().loadZip(zip)
-                        //set the templateVariables
-                    doc.setData(objKey);
-                    doc.render()
-                    var buf = doc.getZip().generate({ type: 'nodebuffer' });
-                    // buf is a nodejs buffer, you can either write it to a file or do anything else with it.
-                    // var randomOutput = 'output-' + Math.floor(Math.random() * Math.floor(100000000000)) + '.docx';
-                    fs.writeFileSync(path.resolve(pathTo, 'export-file-word.docx'), buf);
-                    var result = {
-                        link: 'http://dbdev.namanphu.vn:1357/ageless_sendmail/' + 'export-file-word.docx',
-                        status: Constant.STATUS.SUCCESS,
-                        message: Constant.MESSAGE.ACTION_SUCCESS,
-                    }
-                    res.json(result);
-                }
-            } catch (error) {
-                console.log(error);
-                res.json('Lỗi file export. Vui lòng cầu hình lại!')
-            }
-        });
+app.post('/qlnb/render_automatic_work', async function (req, res) {
+    let body = req.body;
+    var pathFirst = '';
+    var objKey = {};
+    await database.connectDatabase().then(async db => {
+        pathFirst = await getPathFromtblTmplate(db, body.code, body.id)
+        if (body.code == 'ycms') {
+            objKey = await handleRequestShopping(db, body.id)
+        } else if (body.code == 'dntt') {
+            objKey = await handlePaymentOrder(db, body.id)
+        } else {
+            objKey = await handleRequestShopping(db, body.id)
+        }
     })
-    // -------------------------------------------------------------------------------------------------------------------------
+    var pathTo = 'C:/images_services/ageless_sendmail/'
+    console.log(pathTo + pathFirst);
+    fs.readFile(pathTo + pathFirst, 'binary', function (err, data) {
+        try {
+            if (err) {
+                console.log(err);
+                var result = {
+                    status: Constant.STATUS.FAIL,
+                    message: 'File không tồn tại. Vui lòng cầu hình lại!',
+                }
+                res.json(result);
+            } else {
+                var zip = new JSZip(data);
+                var doc = new Docxtemplater().loadZip(zip)
+                //set the templateVariables
+                doc.setData(objKey);
+                doc.render()
+                var buf = doc.getZip().generate({ type: 'nodebuffer' });
+                // buf is a nodejs buffer, you can either write it to a file or do anything else with it.
+                // var randomOutput = 'output-' + Math.floor(Math.random() * Math.floor(100000000000)) + '.docx';
+                fs.writeFileSync(path.resolve(pathTo, 'export-file-word.docx'), buf);
+                var result = {
+                    link: 'http://dbdev.namanphu.vn:1357/ageless_sendmail/' + 'export-file-word.docx',
+                    status: Constant.STATUS.SUCCESS,
+                    message: Constant.MESSAGE.ACTION_SUCCESS,
+                }
+                res.json(result);
+            }
+        } catch (error) {
+            console.log(error);
+            res.json('Lỗi file export. Vui lòng cầu hình lại!')
+        }
+    });
+})
+// -------------------------------------------------------------------------------------------------------------------------
 let routes = require('./api/router') //importing route
 routes(app)
 
@@ -361,8 +361,8 @@ let connect = require('./api/database')
 
 connect.connectDatabase();
 
-const port = process.env.PORT || 3100
-    // wsEngine cho phép gọi vào hàm
+const port = process.env.PORT || 3101
+// wsEngine cho phép gọi vào hàm
 var io = require("socket.io")(server, {
     cors: {
         wsEngine: 'eiows',
@@ -371,19 +371,19 @@ var io = require("socket.io")(server, {
         credentials: true,
     }
 })
-server.listen(port, function() {
+server.listen(port, function () {
     console.log('http://localhost:' + port);
 });
 let scheduleJob = require('./api/scheduleJob')
 scheduleJob.editStatus24HourEveryday()
-    // connect socket
+// connect socket
 var socket = require('./api/socket_io/socket');
 var socket_ts = require('./api/socket_io/socket_ts');
 var socket_ns = require('./api/socket_io/socket_ns');
 socket.sockketIO(io)
 socket_ts.sockketIO(io)
 socket_ns.sockketIO(io)
-app.post('/notification-zalo', async function(req, res) {
+app.post('/notification-zalo', async function (req, res) {
     let body = req.body;
     await socket.socketEmit(io, body.dbname)
     var result = {
@@ -392,7 +392,7 @@ app.post('/notification-zalo', async function(req, res) {
     }
     res.json(result);
 })
-app.post('/notification-kehoach', async function(req, res) {
+app.post('/notification-kehoach', async function (req, res) {
     let body = req.body;
     await socket.socketEmitNotifiPlan(io, body.dbname)
     var result = {
@@ -401,7 +401,7 @@ app.post('/notification-kehoach', async function(req, res) {
     }
     res.json(result);
 })
-app.post('/notification-chiphi', async function(req, res) {
+app.post('/notification-chiphi', async function (req, res) {
     let body = req.body;
     await socket.socketEmitNotifiCost(io, body.dbname)
     var result = {
@@ -410,7 +410,7 @@ app.post('/notification-chiphi', async function(req, res) {
     }
     res.json(result);
 })
-app.post('/notification-yeucau', async function(req, res) {
+app.post('/notification-yeucau', async function (req, res) {
     let body = req.body;
     await socket.socketEmitNotifiRequest(io, body.dbname)
     var result = {
