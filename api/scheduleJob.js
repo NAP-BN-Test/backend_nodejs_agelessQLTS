66f39fe7aeca1c10f6226467830301111999498f
NAP-BN-Test <caonv174@gmail.com>
2021-08-25 17:27:43 +0700

    cập nahajt

--- api/scheduleJob.js
+++ api/scheduleJob.js
@@ -10,6 +10,8 @@ var mtblCurrency = require('./tables/financemanage/tblCurrency')
 var mtblRate = require('./tables/financemanage/tblRate')
 var mtblNghiPhep = require('./tables/hrmanage/tblNghiPhep')
 var mtblDateOfLeave = require('./tables/hrmanage/tblDateOfLeave')
+var mtblChamCong = require('./tables/hrmanage/tblChamCong')
+
 // ghi dữ liệu từ máy chấm công vào database
 async function writeDataFromTimekeeperToDatabase(db, userID, arrayData, month, year, date, staffID) {
     var summaryEndDateS = 0;
@@ -115,8 +117,202 @@ async function writeDataFromTimekeeperToDatabase(db, userID, arrayData, month, y
         await createAttendanceData(db, staffID, datedb, null, 'KL', 'Nghỉ không phép', false, summaryEndDateC)
     }
 }
-const axios = require('axios');
+async function convertNumber(number) {
+    if (number < 10) {
+        return '0' + number
+    } else
+        return number
+}
+var mtblNghiLe = require('./tables/hrmanage/tblNghiLe')
+var mtblConfigWorkday = require('./tables/hrmanage/tblConfigWorkday')
+var mtblLoaiChamCong = require('./tables/hrmanage/tblLoaiChamCong')
 
+async function take7thDataToWork(db, year, month, dateRequest = 10) {
+    //  lấy danh sách thứ 7 đi làm ------------------------------------
+    let array7thDB = []
+    var date = new Date(year, month, 0);
+    var dateFinal = Number(date.toISOString().slice(8, 10))
+    dateFinal += 1
+    if (date != 10)
+        dateFinal = dateRequest
+    let dateWhere = year + '-' + await convertNumber(month) + '-' + await convertNumber(dateFinal)
+    let dateMonthFirst = year + '-' + await convertNumber(month) + '-' + '01'
+    dateWhere = moment(dateWhere).add(7, 'hours')
+    dateMonthFirst = moment(dateMonthFirst).add(7, 'hours')
+    await mtblConfigWorkday(db).findAll({
+        where: {
+            Date: { [Op.between]: [dateMonthFirst, dateWhere] },
+        },
+        order: [
+            ['Date', 'DESC']
+        ],
+    }).then(async data => {
+        if (data.length > 0)
+            for (var saturday = 0; saturday < data.length; saturday++) {
+                var datetConvert = mModules.toDatetimeDay(moment(year + '-' + await convertNumber(month) + '-' + await convertNumber(Number(data[saturday].Date.slice(8, 10)))).add(14, 'hours').format('YYYY-MM-DD HH:mm:ss.SSS'))
+                if (datetConvert.slice(0, 5) == 'Thứ 7')
+                    array7thDB.push(Number(data[saturday].Date.slice(8, 10)))
+            }
+    })
+    return array7thDB
+    // ----------------------------------------------------------------------
+}
+const axios = require('axios');
+async function getListHoliday(db, year, month, dateFinal) {
+    let arrayResult = []
+    await mtblNghiLe(db).findAll({
+        where: {
+            [Op.or]: {
+                DateStartHoliday: {
+                    [Op.substring]: year + '-' + await convertNumber(month)
+                },
+                DateEndHoliday: {
+                    [Op.substring]: year + '-' + await convertNumber(month)
+                },
+            },
+        }
+    }).then(data => {
+        data.forEach(element => {
+            let dateStart = moment(element.DateStartHoliday).add(7, 'hours').date()
+            let dateEnd = moment(element.DateEndHoliday).add(7, 'hours').date()
+            let dateEndMonth = moment(element.DateEndHoliday).add(7, 'hours').month()
+            dateEndMonth += 1
+            if (dateEndMonth != month) {
+                dateEnd = dateFinal
+            }
+            for (var i = dateStart; i <= dateEnd; i++) {
+                arrayResult.push(i)
+            }
+        })
+    })
+    return arrayResult
+}
+// Tạo dữ liệu chấm công
+async function createAttendanceData(db, staffID, date, Time, status, reason, type, SummaryEndDate) {
+    await mtblChamCong(db).create({
+        IDNhanVien: staffID,
+        Date: date,
+        Time: Time,
+        Status: status,
+        Reason: reason,
+        Type: type,
+        SummaryEndDate: SummaryEndDate,
+    })
+
+}
+async function getListleaveDate(db, month, year, staffID, dateFinal) {
+    let objResult = {}
+    var array = [];
+    var arrayObj = [];
+    let listID = []
+    let arrayRegimeLeave = [];
+    let arrayO = [];
+    let arrayKL = [];
+    let tblNghiPhep = mtblNghiPhep(db);
+    let ListSign = [];
+    await tblNghiPhep.findAll({
+        where: {
+            IDNhanVien: staffID,
+            Status: 'Hoàn thành',
+        },
+    }).then(leave => {
+        leave.forEach(item => {
+            listID.push(item.ID)
+        })
+    })
+    for (let i = 0; i < listID.length; i++) {
+        let query = `SELECT [ID], [LeaveID], [DateEnd], [DateStart], [IDLoaiChamCong] FROM [tblDateOfLeave] AS [tblDateOfLeave] 
+        WHERE (DATEPART(yy, [tblDateOfLeave].[DateEnd]) = ` + year + ` AND DATEPART(mm, [tblDateOfLeave].[DateEnd]) = ` + month + `) AND ([tblDateOfLeave].[LeaveID] = N'` + listID[i] + `');`
+        let date = await db.query(query)
+        date = date[0]
+        for (let i = 0; i < date.length; i++) {
+            if (date[i].IDLoaiChamCong) {
+                let signLeave = ''
+                let dateStart = moment(date[i].DateStart).subtract(7, 'hours').date()
+                let dateEnd = moment(date[i].DateEnd).subtract(7, 'hours').date()
+                let dateEndMonth = moment(date[i].DateEnd).subtract(7, 'hours').month()
+                // lấy tháng bị trừ 1
+                let signObj = await mtblLoaiChamCong(db).findOne({ where: { ID: date[i].IDLoaiChamCong } })
+                signLeave = signObj ? signObj.Code : ''
+                if (signObj && signObj.Code == 'O' || signObj.Code == 'CT' || signObj.Code == 'TS' || signObj.Code == 'CÐ') {
+                    arrayRegimeLeave.push(signObj.Code)
+                }
+                if (signObj && signObj.Code == 'O') {
+                    arrayO.push(signObj.Code)
+                }
+                if (signObj && signObj.Code == 'KL') {
+                    arrayKL.push(signObj.Code)
+                }
+                dateEndMonth += 1
+                if (dateEndMonth != month) {
+                    dateEnd = dateFinal
+                } else {
+                    if (Number(moment(date[i].DateEnd).subtract(7, 'hours').format('HH') <= 8)) {
+                        dateEnd -= 1
+                    }
+                }
+                for (let d = dateStart; d <= dateEnd; d++) {
+
+                    array.push(d)
+                    arrayObj.push({
+                        date: d,
+                        id: date[i].ID,
+                        sign: signLeave,
+                    })
+                }
+            }
+
+        }
+
+    }
+    objResult = {
+        array: array,
+        arrayObj: arrayObj,
+        arrayRegimeLeave: arrayRegimeLeave,
+        arrayKL: arrayKL,
+        arrayO: arrayO,
+    }
+    return objResult
+}
+function checkDuplicate(array, elm) {
+    var check = false;
+    array.forEach(item => {
+        if (item === elm) check = true;
+    })
+    return check;
+}
+async function createDataTimeKeeping(db, year, month, date, staffID, IDMayChamCong, arrayData) {
+    var dateFi = new Date(year, month, 0);
+    var dateFinal = Number(dateFi.toISOString().slice(8, 10))
+    dateFinal += 1
+    var arrayHoliday = await getListHoliday(db, year, month, dateFinal)
+    var array7thDB = await take7thDataToWork(db, year, month);
+    var datetConvert = mModules.toDatetimeDay(moment(year + '-' + await convertNumber(month) + '-' + await convertNumber(date)).add(14, 'hours').format('YYYY-MM-DD HH:mm:ss.SSS'))
+    let dateMonth = moment(year + '/' + await convertNumber(month) + ' / ' + await convertNumber(date)).add(7, 'hours').format('YYYY/MM/DD HH:MM:SS')
+    if (datetConvert.slice(0, 8) == 'Chủ nhật') {
+        await createAttendanceData(db, staffID, dateMonth, null, 'Sun', 'Nghỉ chủ nhật', true, 0)
+        await createAttendanceData(db, staffID, dateMonth, null, 'Sun', 'Nghỉ chủ nhật', false, 0)
+    } else if (datetConvert.slice(0, 5) == 'Thứ 7' && !checkDuplicate(array7thDB, date)) {
+        await createAttendanceData(db, staffID, dateMonth, null, 'Sat', 'Nghỉ thứ bảy', true, 0)
+        await createAttendanceData(db, staffID, dateMonth, null, 'Sat', 'Nghỉ thứ bảy', false, 0)
+    } else if (checkDuplicate(arrayHoliday, date)) {
+        await createAttendanceData(db, staffID, dateMonth, null, 'Holiday', 'Nghỉ lễ', true, 0)
+        await createAttendanceData(db, staffID, dateMonth, null, 'Holiday', 'Nghỉ lễ', false, 0)
+    } else {
+        var arrayLeaveDay = await getListleaveDate(db, month, year, staffID, dateFinal)
+        // check xem có trong ngày nghỉ phép không ?
+        if (checkDuplicate(arrayLeaveDay.array, date)) {
+            for (let i = 0; i < arrayLeaveDay.arrayObj.length; i++) {
+                if (arrayLeaveDay.arrayObj[i].date == date) {
+                    await createAttendanceData(db, staffID, dateMonth, null, arrayLeaveDay.arrayObj[i].sign, 'Nghỉ phép', false, 0)
+                    await createAttendanceData(db, staffID, dateMonth, null, arrayLeaveDay.arrayObj[i].sign, 'Nghỉ phép', true, 0)
+                }
+            }
+        } else {
+            await writeDataFromTimekeeperToDatabase(db, IDMayChamCong, arrayData, month, year, date, staffID)
+        }
+    }
+}
 module.exports = {
     editStatus24HourEveryday: async (req, res) => {
         database.connectDatabase().then(async db => {
@@ -160,6 +356,7 @@ module.exports = {
                             IDMayChamCong: arrayData[dataTimeKp]['User ID']
                         })
                         if (staff) {
+                            let Time = moment(arrayData[dataTimeKp]['Verify Date'], 'YYYY-M-D h:m:s').format('YYYY-MM-DD hh:mm:ss');
                             let checkMonth = moment(arrayData[dataTimeKp]['Verify Date'], 'YYYY-M').format('YYYY-MM');
                             let checkTimekeeping = await mtblChamCong(db).findOne({
                                 where: {
@@ -170,7 +367,7 @@ module.exports = {
                                 let date = moment(arrayData[dataTimeKp]['Verify Date'], 'YYYY-M-D h:m:s').format('DD')
                                 let month = moment(arrayData[dataTimeKp]['Verify Date'], 'YYYY-M-D h:m:s').format('MM')
                                 let year = moment(arrayData[dataTimeKp]['Verify Date'], 'YYYY-M-D h:m:s').format('YYYY')
-                                await writeDataFromTimekeeperToDatabase(db, arrayData[dataTimeKp]['User ID'], arrayData, month, year, date, staff.ID);
+                                await createDataTimeKeeping(db, year, month, date, staff.ID, arrayData[dataTimeKp]['User ID'], arrayData);
                             }
                         }
                     }
@@ -223,6 +420,7 @@ module.exports = {
                             IDMayChamCong: arrayData[dataTimeKp]['User ID']
                         })
                         if (staff) {
+                            let Time = moment(arrayData[dataTimeKp]['Verify Date'], 'YYYY-M-D h:m:s').format('YYYY-MM-DD hh:mm:ss');
                             let checkMonth = moment(arrayData[dataTimeKp]['Verify Date'], 'YYYY-M').format('YYYY-MM');
                             let checkTimekeeping = await mtblChamCong(db).findOne({
                                 where: {
@@ -233,7 +431,7 @@ module.exports = {
                                 let date = moment(arrayData[dataTimeKp]['Verify Date'], 'YYYY-M-D h:m:s').format('DD')
                                 let month = moment(arrayData[dataTimeKp]['Verify Date'], 'YYYY-M-D h:m:s').format('MM')
                                 let year = moment(arrayData[dataTimeKp]['Verify Date'], 'YYYY-M-D h:m:s').format('YYYY')
-                                await writeDataFromTimekeeperToDatabase(db, arrayData[dataTimeKp]['User ID'], arrayData, month, year, date, staff.ID);
+                                await createDataTimeKeeping(db, year, month, date, staff.ID, arrayData[dataTimeKp]['User ID'], arrayData);
                             }
                         }
                     }
